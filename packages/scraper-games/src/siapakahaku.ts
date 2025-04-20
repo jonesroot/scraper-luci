b72a1ab7e056ed384e50c9cfa9aeab905cd00afa
Lucifer <38715155+jonesroot@users.noreply.github.com>
2025-04-20 13:16:36 +0700

    update packages/scraper-games/src/siapakahaku.ts

--- packages/scraper-games/src/siapakahaku.ts
+++ packages/scraper-games/src/siapakahaku.ts
@@ -5,7 +5,7 @@ export let siapakahakujson: SiapakahAku[]
 export default async function siapakahaku (): Promise<SiapakahAku> {
   if (!siapakahakujson) {
     siapakahakujson = await got(
-      'https://raw.githubusercontent.com/BochilTeam/database/master/games/siapakahaku.json'
+      'https://raw.githubusercontent.com/jonesroot/database/master/games/siapakahaku.json'
     ).json()
   }
   return SiapakahAkuSchema.parse(
