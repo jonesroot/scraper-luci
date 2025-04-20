0de1fb60e998cb687d79c8e28c0ae2e7703753b4
Lucifer <38715155+jonesroot@users.noreply.github.com>
2025-04-20 13:16:09 +0700

    update packages/scraper-games/src/tebakkimia.ts

--- packages/scraper-games/src/tebakkimia.ts
+++ packages/scraper-games/src/tebakkimia.ts
@@ -5,7 +5,7 @@ export let tebakkimiajson: TebakKimia[]
 export default async function tebakkimia (): Promise<TebakKimia> {
   if (!tebakkimiajson) {
     tebakkimiajson = await got(
-      'https://raw.githubusercontent.com/BochilTeam/database/master/games/tebakkimia.json'
+      'https://raw.githubusercontent.com/jonesroot/database/master/games/tebakkimia.json'
     ).json()
   }
   return TebakKimiaSchema.parse(
